d50ea12c74d40918838615f9fa7b00957ee26ea6
Tristan Brindle <t.c.brindle@gmail.com>
2022-06-07 17:47:18 +0100

    Add sort benchmark
    Sample result on my system with GCC 11.3:
    
    | relative |               ns/op |                op/s |    err% |     total | benchmark
    |---------:|--------------------:|--------------------:|--------:|----------:|:----------
    |   100.0% |        5,546,800.00 |              180.28 |    1.7% |      0.68 | `random ints (std)`
    |   203.2% |        2,729,800.00 |              366.33 |    2.5% |      0.33 | `random ints (flux)`
    |   100.0% |          773,250.00 |            1,293.24 |    1.2% |      0.09 | `sorted ints (std)`
    |   916.2% |           84,400.00 |           11,848.34 |    4.8% |      0.01 | `sorted ints (flux)`
    |   100.0% |          572,583.33 |            1,746.47 |    2.1% |      0.07 | `reverse sorted ints (std)`
    |   327.4% |          174,909.09 |            5,717.26 |    1.9% |      0.02 | `reverse sorted ints (flux)`
    |   100.0% |        5,381,454.55 |              185.82 |    1.8% |      0.67 | `organpipe ints (std)`
    |   190.8% |        2,820,700.00 |              354.52 |    1.2% |      0.36 | `organpipe ints (flux)`
    |   100.0% |        6,131,300.00 |              163.10 |    1.5% |      0.75 | `random doubles (std)`
    |   210.0% |        2,919,000.00 |              342.58 |    1.0% |      0.37 | `random doubles (flux)`
    |   100.0% |       23,965,000.00 |               41.73 |    3.0% |      0.27 | `random strings (std)`
    |   104.9% |       22,856,000.00 |               43.75 |    4.0% |      0.25 | `random strings (flux)`

---
+++ benchmark/sort_benchmark.cpp
@@ -0,0 +1,89 @@
+
+#include "nanobench.h"
+
+#include <flux.hpp>
+
+#include <algorithm>
+#include <numeric>
+#include <random>
+
+namespace an = ankerl::nanobench;
+
+static constexpr int test_sz = 100'000;
+
+template <typename SortFn, typename Vec>
+static void test_sort(const char* name, SortFn, const Vec& vec, an::Bench& bench)
+{
+    bench.run(name, [&] {
+        Vec copy = vec;
+        SortFn{}(copy);
+        bench.doNotOptimizeAway(copy);
+    });
+}
+
+int main()
+{
+    {
+        std::vector<int> vec(test_sz);
+        std::mt19937 gen{std::random_device{}()};
+        std::uniform_int_distribution dist(0, test_sz);
+        std::generate(vec.begin(), vec.end(), [&] { return dist(gen); });
+
+        auto bench = an::Bench().relative(true).minEpochIterations(10);
+
+        test_sort("random ints (std)", std::ranges::sort, vec, bench);
+        test_sort("random ints (flux)", flux::sort, vec, bench);
+    }
+
+    {
+        std::vector<int> vec(test_sz);
+        std::iota(vec.begin(), vec.end(), 0);
+        auto bench = an::Bench().relative(true).minEpochIterations(10);
+        test_sort("sorted ints (std)", std::ranges::sort, vec, bench);
+        test_sort("sorted ints (flux)", flux::sort, vec, bench);
+    }
+
+    {
+        std::vector<int> vec(test_sz);
+        std::iota(vec.begin(), vec.end(), 0);
+        std::ranges::reverse(vec);
+        auto bench = an::Bench().relative(true).minEpochIterations(10);
+        test_sort("reverse sorted ints (std)", std::ranges::sort, vec, bench);
+        test_sort("reverse sorted ints (flux)", flux::sort, vec, bench);
+    }
+
+    {
+        std::vector<int> vec(test_sz);
+        std::iota(vec.begin(), vec.begin() + test_sz/2, 0);
+        std::iota(vec.begin() + test_sz/2, vec.end(), 0);
+        std::reverse(vec.begin() + test_sz/2, vec.end());
+
+        auto bench = an::Bench().relative(true).minEpochIterations(10);
+        test_sort("organpipe ints (std)", std::ranges::sort, vec, bench);
+        test_sort("organpipe ints (flux)", flux::sort, vec, bench);
+    }
+
+    {
+        std::vector<double> vec(test_sz);
+        std::mt19937 gen{std::random_device{}()};
+        std::uniform_real_distribution<double> dist(0, test_sz);
+        std::generate(vec.begin(), vec.end(), [&] { return dist(gen); });
+
+        auto bench = an::Bench().relative(true).minEpochIterations(10);
+
+        test_sort("random doubles (std)", std::ranges::sort, vec, bench);
+        test_sort("random doubles (flux)", flux::sort, vec, bench);
+    }
+
+    {
+        std::vector<std::string> vec(test_sz);
+        std::mt19937 gen{std::random_device{}()};
+        std::uniform_int_distribution dist(0, test_sz);
+        std::generate(vec.begin(), vec.end(), [&] { return std::to_string(dist(gen)); });
+
+        auto bench = an::Bench().relative(true);
+
+        test_sort("random strings (std)", std::ranges::sort, vec, bench);
+        test_sort("random strings (flux)", flux::sort, vec, bench);
+    }
+}
