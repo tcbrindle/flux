31e2c564c13ea3f0876b213493d16fd7079002ec
Tristan Brindle <t.c.brindle@gmail.com>
2022-12-05 16:32:35 +0000

    Add seq._(fn, args...) member function
    This allows adding custom operations as part of a pipeline using the "dot" syntax, for example
    
        auto res = get_sequence()
                    .filter(...)
                    .map(...)
                    ._(custom_op, arg1, arg2)
                    .fold(...)
    
    which will call `custom_user_op(seq, arg1, arg2)` with the adapted sequence as the first argument, followed by the provided args.
    
    I previously had this in Flow as `x.apply(fn, args...)`, but Barry's idea from his Rivers library to name it just `x._(fn, args...)` is much better.

---
+++ test/test_apply.cpp
@@ -0,0 +1,68 @@
+
+#include "catch.hpp"
+
+#include <flux.hpp>
+
+#include <array>
+#include <algorithm>
+
+#include "test_utils.hpp"
+
+// We don't have an in-place rotate yet, so let's borrow the STL's
+auto rotate_by = []<flux::random_access_sequence Seq>(Seq&& seq, flux::distance_t places)
+    -> Seq&&
+{
+    auto const sz = flux::size(seq);
+
+    while (places < 0) {
+        places += sz;
+    }
+    while (places >= sz) {
+        places -= sz;
+    }
+
+    auto view = flux::view(seq);
+    std::ranges::rotate(view, view.begin() + places);
+
+    return FLUX_FWD(seq);
+};
+
+
+constexpr bool test_apply()
+{
+    // Checking lvalue sequence
+    {
+        auto seq = flux::from(std::array{1, 2, 3, 4, 5});
+        auto& r = seq._(rotate_by, -1);
+        STATIC_CHECK(&r == &seq);
+        STATIC_CHECK(check_equal(seq, {5, 1, 2, 3, 4}));
+    }
+
+    // Checking rvalue sequence
+    {
+        auto seq = flux::from(std::array{1, 2, 3, 4, 5})._(rotate_by, -1).take(3);
+        STATIC_CHECK(check_equal(seq, {5, 1, 2}));
+    }
+
+    // Checking const [l|r]value
+    {
+        auto sum = [](auto& seq) {
+            int s = 0;
+            FLUX_FOR(int i, seq) { s += i; }
+            return s;
+        };
+
+        auto const seq = flux::from(std::array{1, 2, 3, 4, 5});
+        STATIC_CHECK(seq._(sum) == 15);
+
+        STATIC_CHECK(std::move(seq)._(sum) == 15);
+    }
+
+    return true;
+}
+static_assert(test_apply());
+
+TEST_CASE("apply")
+{
+    REQUIRE(test_apply());
+}
