b7ae19b3ea1880761053c4802980ea72fbbf33fa
Tristan Brindle <t.c.brindle@gmail.com>
2023-01-09 18:33:57 +0000

    Add config options for signed overflow behavior
    Either "error" (throw or terminate), "wrap" (give the wrong answer, but no UB), or "ignore" (on your head be it)

--- include/flux/core/config.hpp
+++ include/flux/core/config.hpp
@@ -20,6 +20,13 @@
 // Default error policy is terminate
 #define FLUX_DEFAULT_ERROR_POLICY FLUX_ERROR_POLICY_TERMINATE
 
+// Default overflow policy is error in debug builds, wrap in release builds
+#ifdef NDEBUG
+#  define FLUX_DEFAULT_OVERFLOW_POLICY FLUX_OVERFLOW_POLICY_WRAP
+#else
+#  define FLUX_DEFAULT_OVERFLOW_POLICY FLUX_OVERFLOW_POLICY_ERROR
+#endif // NDEBUG
+
 // Select which error policy to use
 #if defined(FLUX_TERMINATE_ON_ERROR)
 #  define FLUX_ERROR_POLICY FLUX_ERROR_POLICY_TERMINATE
@@ -33,6 +40,17 @@
 #ifndef FLUX_PRINT_ERROR_ON_TERMINATE
 #  define FLUX_PRINT_ERROR_ON_TERMINATE 1
 #endif // FLUX_PRINT_ERROR_ON_TERMINATE
+
+// Select which overflow policy to use
+#if defined(FLUX_ERROR_ON_OVERFLOW)
+#  define FLUX_OVERFLOW_POLICY FLUX_OVERFLOW_POLICY_ERROR
+#elif defined(FLUX_WRAP_ON_OVERFLOW)
+#  define FLUX_OVERFLOW_POLICY FLUX_OVERFLOW_POLICY_WRAP
+#elif defined(FLUX_IGNORE_OVERFLOW)
+#  define FLUX_OVERFLOW_POLICY FLUX_OVERFLOW_POLICY_IGNORE
+#else
+#  define FLUX_OVERFLOW_POLICY FLUX_DEFAULT_OVERFLOW_POLICY
+#endif // FLUX_ERROR_ON_OVERFLOW
 
 // Default int_t is ptrdiff_t
 #define FLUX_DEFAULT_INT_TYPE std::ptrdiff_t
@@ -49,6 +67,12 @@ enum class error_policy {
     unwind = FLUX_ERROR_POLICY_UNWIND
 };
 
+enum class overflow_policy {
+    ignore = FLUX_OVERFLOW_POLICY_IGNORE,
+    wrap = FLUX_OVERFLOW_POLICY_WRAP,
+    error = FLUX_OVERFLOW_POLICY_ERROR
+};
+
 namespace config {
 
 using int_type = FLUX_INT_TYPE;
@@ -56,6 +80,8 @@ static_assert(std::signed_integral<int_type> && (sizeof(int_type) >= sizeof(int)
               "Custom FLUX_INT_TYPE must be a signed integer type at least as large as int");
 
 inline constexpr error_policy on_error = static_cast<error_policy>(FLUX_ERROR_POLICY);
+
+inline constexpr overflow_policy on_overflow = static_cast<overflow_policy>(FLUX_OVERFLOW_POLICY);
 
 inline constexpr bool print_error_on_terminate = FLUX_PRINT_ERROR_ON_TERMINATE;
 
