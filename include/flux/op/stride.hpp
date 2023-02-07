dd1a7f59c276d50fa8614a6f42036010ee3ffbdc
Tristan Brindle <t.c.brindle@gmail.com>
2023-02-07 14:57:18 +0000

    Add stride() impl for bidirectional sequences
    For bidirectional sequences, stride is a bit more complicated, because we want to make sure that
    
        auto seq = flux::stride(std::vector{1, 2, 3, 4, 5}, 2).reverse();
    
    yields the correct [5, 3, 1], rather than simply starting at the end and striding backwards (which would yield [4, 2]).
    
    To do this, we use the same design as P1899, namely keeping track of the number of "missing" elements from within our cursor. This means that there is a bit of overhead when using stride() with bidir+ sequences, even if you don't actually want to iterate the resulting sequence backwards. Perhaps some way of limiting the capabilities of sequences (as we have in some of the tests) would be worth investigating.

--- include/flux/op/stride.hpp
+++ include/flux/op/stride.hpp
@@ -112,6 +112,141 @@ public:
     };
 };
 
+template <bidirectional_sequence Base>
+struct stride_adaptor<Base> : inline_sequence_base<stride_adaptor<Base>> {
+private:
+    Base base_;
+    distance_t stride_;
+
+public:
+    constexpr stride_adaptor(decays_to<Base> auto&& base, distance_t stride)
+        : base_(FLUX_FWD(base)),
+          stride_(stride)
+    {}
+
+    struct flux_sequence_traits {
+    private:
+        struct cursor_type {
+            cursor_t<Base> cur{};
+            distance_t missing = 0;
+
+            friend constexpr auto operator==(cursor_type const& lhs, cursor_type const& rhs) -> bool
+            {
+                return lhs.cur == rhs.cur;
+            }
+
+            friend constexpr auto operator<=>(cursor_type const& lhs, cursor_type const& rhs)
+            {
+                return lhs.cur <=> rhs.cur;
+            }
+        };
+
+    public:
+        using value_type = value_t<Base>;
+        static constexpr bool is_infinite = infinite_sequence<Base>;
+
+        static constexpr auto first(auto& self) -> cursor_type
+        {
+            return cursor_type {
+                .cur = flux::first(self.base_),
+                .missing = 0
+            };
+        }
+
+        static constexpr auto is_last(auto& self, cursor_type const& cur) -> bool
+        {
+            return flux::is_last(self.base_, cur.cur);
+        }
+
+        static constexpr auto inc(auto& self, cursor_type& cur) -> void
+        {
+            cur.missing = advance(self.base_, cur.cur, self.stride_);
+        }
+
+        static constexpr auto read_at(auto& self, cursor_type const& cur)
+            -> decltype(flux::read_at(self.base_, cur.cur))
+        {
+            return flux::read_at(self.base_, cur.cur);
+        }
+
+        static constexpr auto move_at(auto& self, cursor_type const& cur)
+            -> decltype(flux::move_at(self.base_, cur.cur))
+        {
+            return flux::move_at(self.base_, cur.cur);
+        }
+
+        static constexpr auto read_at_unchecked(auto& self, cursor_type const& cur)
+            -> decltype(flux::read_at_unchecked(self.base_, cur.cur))
+        {
+            return flux::read_at_unchecked(self.base_, cur.cur);
+        }
+
+        static constexpr auto move_at_unchecked(auto& self, cursor_type const& cur)
+            -> decltype(flux::move_at_unchecked(self.base_, cur.cur))
+        {
+            return flux::move_at_unchecked(self.base_, cur.cur);
+        }
+
+        static constexpr auto last(auto& self) -> cursor_type
+            requires bounded_sequence<Base> && sized_sequence<Base>
+        {
+            distance_t missing =
+                (self.stride_ - flux::size(self.base_) % self.stride_) % self.stride_;
+            return cursor_type{
+                .cur = flux::last(self.base_),
+                .missing = missing
+            };
+        }
+
+        static constexpr auto dec(auto& self, cursor_type& cur) -> void
+            requires bidirectional_sequence<Base>
+        {
+            advance(self.base_, cur.cur, cur.missing - self.stride_);
+            cur.missing = 0;
+        }
+
+        static constexpr auto size(auto& self) -> distance_t
+            requires sized_sequence<Base>
+        {
+            auto s = flux::size(self.base_);
+            return s/self.stride_ + (s % self.stride_ == 0 ? 0 : 1);
+        }
+
+        static constexpr auto distance(auto& self, cursor_type const& from,
+                                       cursor_type const& to) -> distance_t
+            requires random_access_sequence<Base>
+        {
+            return (flux::distance(self.base_, from.cur, to.cur) - from.missing + to.missing)/self.stride_;
+        }
+
+        static constexpr auto inc(auto& self, cursor_type& cur, distance_t offset) -> void
+            requires random_access_sequence<Base>
+        {
+            if (offset > 0) {
+                cur.missing = advance(self.base_, cur.cur, num::checked_mul(offset, self.stride_)) % self.stride_;
+            } else if (offset < 0) {
+                advance(self.base_, cur.cur, num::checked_add(num::checked_mul(offset, self.stride_), cur.missing));
+                cur.missing = 0;
+            }
+        }
+
+        static constexpr auto for_each_while(auto& self, auto&& pred) -> cursor_type
+            requires sequence<decltype((self.base_))>
+        {
+            distance_t n = self.stride_;
+            auto c = flux::for_each_while(self.base_, [&n, &pred, s = self.stride_](auto&& elem) {
+                if (++n < s) {
+                    return true;
+                } else {
+                    n = 0;
+                    return std::invoke(pred, FLUX_FWD(elem));
+                }
+            });
+            return cursor_type{std::move(c), (n + 1) % self.stride_};
+        }
+    };
+};
+
 struct stride_fn {
     template <adaptable_sequence Seq>
     [[nodiscard]]
--- test/test_stride.cpp
+++ test/test_stride.cpp
@@ -11,6 +11,7 @@
 
 #include <array>
 #include <limits>
+#include <list>
 
 namespace {
 
@@ -129,10 +130,222 @@ constexpr bool test_stride_non_bidir()
 }
 static_assert(test_stride_non_bidir());
 
+constexpr bool test_stride_bidir()
+{
+    // Basic stride, n divides size
+    {
+        std::array arr{0, 1, 2, 3, 4, 5};
+
+        auto seq = flux::stride(arr, 2);
+
+        using S = decltype(seq);
+        static_assert(flux::sequence<S>);
+        static_assert(flux::multipass_sequence<S>);
+        static_assert(flux::bounded_sequence<S>);
+        static_assert(flux::random_access_sequence<S>);
+        static_assert(not flux::contiguous_sequence<S>);
+        static_assert(flux::sized_sequence<S>);
+
+        STATIC_CHECK(check_equal(seq, {0, 2, 4}));
+        STATIC_CHECK(seq.last().cur == flux::last(arr));
+        STATIC_CHECK(seq.size() == 3);
+        STATIC_CHECK(seq.distance(seq.first(), seq.last()) == 3);
+        STATIC_CHECK(seq.distance(seq.last(), seq.first()) == -3);
+    }
+
+    // Basic stride, n does not divide size
+    {
+        std::array arr{0, 1, 2, 3, 4, 5, 6, 7};
+
+        auto const seq = flux::stride(arr,3);
+
+        using S = decltype(seq);
+        static_assert(flux::sequence<S>);
+        static_assert(flux::multipass_sequence<S>);
+        static_assert(flux::bounded_sequence<S>);
+        static_assert(flux::random_access_sequence<S>);
+        static_assert(flux::sized_sequence<S>);
+
+        STATIC_CHECK(check_equal(seq, {0, 3, 6}));
+        STATIC_CHECK(flux::last(seq).cur == flux::last(arr));
+        STATIC_CHECK(flux::size(seq) == 3);
+        STATIC_CHECK(flux::distance(seq, flux::first(seq), flux::last(seq)) == 3);
+        STATIC_CHECK(flux::distance(seq, flux::last(seq), flux::first(seq)) == -3);
+    }
+
+    // Reversing gives the expected result
+    {
+        // stride divides size
+        {
+            std::array arr{0, 1, 2, 3, 4, 5};
+
+            auto stride_then_rev = flux::stride(arr, 3).reverse();
+            auto rev_then_stride = flux::reverse(arr).stride(3);
+
+            STATIC_CHECK(check_equal(stride_then_rev, {3, 0}));
+            STATIC_CHECK(check_equal(rev_then_stride, {5, 2}));
+        }
+
+        // stride does not divide size
+        {
+            std::array arr{0, 1, 2, 3, 4, 5, 6};
+
+            auto stride_then_rev = flux::stride(arr, 3).reverse();
+            auto rev_then_stride = flux::reverse(arr).stride(3);
+
+            STATIC_CHECK(check_equal(stride_then_rev, {6, 3, 0}));
+            STATIC_CHECK(check_equal(rev_then_stride, {6, 3, 0}));
+        }
+
+        // ...and again
+        {
+            std::array arr{0, 1, 2, 3, 4, 5, 6, 7};
+
+            auto stride_then_rev = flux::stride(arr, 3).reverse();
+            auto rev_then_stride = flux::reverse(arr).stride(3);
+
+            STATIC_CHECK(check_equal(stride_then_rev, {6, 3, 0}));
+            STATIC_CHECK(check_equal(rev_then_stride, {7, 4, 1}));
+        }
+    }
+
+    // Stride of 1 returns the original sequence
+    {
+        std::array arr{0, 1, 2, 3, 4, 5, 6};
+
+        auto const seq = flux::stride(arr, 1);
+
+        STATIC_CHECK(check_equal(seq, arr));
+        STATIC_CHECK(check_equal(flux::reverse(seq), flux::reverse(arr)));
+    }
+
+    // Stride >= than sequence size returns sequence of one element
+    {
+        {
+            std::array arr{0, 1, 2, 3, 4, 5};
+            auto seq = flux::ref(arr).stride(arr.size());
+
+            auto cur = seq.first();
+            STATIC_CHECK(!seq.is_last(cur));
+            STATIC_CHECK(seq[cur] == 0);
+            cur = seq.next(cur);
+            STATIC_CHECK(seq.is_last(cur));
+        }
+
+        {
+            std::array arr{0, 1, 2, 3, 4, 5};
+            auto seq = flux::ref(arr).stride(99999);
+
+            auto cur = seq.first();
+            STATIC_CHECK(!seq.is_last(cur));
+            STATIC_CHECK(seq[cur] == 0);
+            cur = seq.next(cur);
+            STATIC_CHECK(seq.is_last(cur));
+        }
+    }
+
+    // Stride of distance_t::max doesn't break stuff
+    {
+        std::array arr{0, 1, 2, 3, 4, 5};
+        auto seq = flux::ref(arr).stride(std::numeric_limits<flux::distance_t>::max());
+
+        auto cur = seq.first();
+        STATIC_CHECK(!seq.is_last(cur));
+        STATIC_CHECK(seq[cur] == 0);
+        cur = seq.next(cur);
+        STATIC_CHECK(seq.is_last(cur));
+    }
+
+    // In-bounds RA jumps work as expected
+    {
+        std::array arr{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        auto seq = flux::stride(arr, 3);
+
+        auto cur = seq.first();
+        seq.inc(cur, 2);
+
+        STATIC_CHECK(seq[cur] = 6);
+
+        seq.inc(cur, -2);
+
+        STATIC_CHECK(seq[cur] == 0);
+    }
+
+    // Out-of-bounds RA jumps are saturating
+    {
+        std::array arr{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        auto seq = flux::stride(arr, 3);
+
+        auto cur = seq.first();
+        seq.inc(cur, 10);
+
+        STATIC_CHECK(cur.cur == 10);
+        STATIC_CHECK(seq.is_last(cur));
+
+        seq.inc(cur, -2);
+
+        STATIC_CHECK(seq[cur] == 6);
+
+        cur = seq.first();
+        seq.inc(cur, -1);
+        STATIC_CHECK(seq[cur] == 0);
+    }
+
+    // Internal iteration works as expected
+    {
+        std::array arr{0, 1, 2, 3, 4, 5, 6};
+
+        auto seq = flux::ref(arr).stride(2);
+
+        STATIC_CHECK(seq.sum() == 0 + 2 + 4 + 6);
+
+        auto cur = seq.find(4);
+
+        STATIC_CHECK(&seq[cur] == arr.data() + 4);
+
+        cur = seq.find(99999);
+        STATIC_CHECK(seq.is_last(cur));
+        seq.dec(cur);
+
+        STATIC_CHECK(&seq[cur] == arr.data() + 6);
+    }
+
+
+    // Can we sort a strided array?
+    {
+        std::array arr{9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        flux::ref(arr).stride(3).sort();
+
+        STATIC_CHECK(check_equal(arr, {3, 8, 7, 6, 5, 4, 9, 2, 1}));
+    }
+
+    return true;
+}
+static_assert(test_stride_bidir());
+
 }
 
 TEST_CASE("stride")
 {
-    auto res = test_stride_non_bidir();
+    bool res = test_stride_non_bidir();
     REQUIRE(res);
+
+    res = test_stride_bidir();
+    REQUIRE(res);
+
+    // Test with bidir-but-not-RA sequence
+    {
+        auto list = std::list<int>{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        auto seq = flux::from_range(list).stride(3);
+
+        REQUIRE(seq.size() == 3);
+        REQUIRE(check_equal(seq, {1, 4, 7}));
+        REQUIRE(seq.sum() == 12);
+
+        auto rev = std::move(seq).reverse();
+
+        REQUIRE(check_equal(rev, {7, 4, 1}));
+        REQUIRE(rev.sum() == 12);
+    }
 }
