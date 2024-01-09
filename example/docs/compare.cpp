ea6a454fbe64cb407cb98a5f24d334c80317e64c
Tristan Brindle <t.c.brindle@gmail.com>
2024-01-09 13:38:03 +0000

    Enable asserts in release mode for doc examples
    Because (a) the asserts are kind of the whole point, and (b) it removes a load of unused variable warnings
    
    But do it carefully so that we don't change any of the parts that are imported in the docs themselves...

--- example/docs/compare.cpp
+++ example/docs/compare.cpp
@@ -5,7 +5,7 @@
 
 #include <flux.hpp>
 
-#include <cassert>
+#include "assert.hpp"
 #include <cmath>
 #include <limits>
 #include <vector>
--- example/docs/find_max.cpp
+++ example/docs/find_max.cpp
@@ -1,7 +1,7 @@
 
 #include <flux.hpp>
 
-#include <cassert>
+#include "assert.hpp"
 #include <string>
 #include <vector>
 
