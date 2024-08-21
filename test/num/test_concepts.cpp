8d9f18699cefbe64e19bd327084340754119e1bf
Tristan Brindle <t.c.brindle@gmail.com>
2024-08-21 20:04:33 +0100

    Don't try testing extended integer types
    Results seem to be too inconsistent on CI

--- test/num/test_concepts.cpp
+++ test/num/test_concepts.cpp
@@ -5,10 +5,6 @@
 
 #include "../test_utils.hpp"
 
-#ifdef __clang__
-#define COMPILER_IS_CLANG 1
-#endif
-
 /*
  * Built-in signed integers
  */
@@ -67,31 +63,3 @@ static_assert(not flux::num::integral<double>);
 static_assert(not flux::num::integral<long double>);
 static_assert(not flux::num::integral<std::byte>);
 
-/*
- * Compiler-provided extra builtin integer types
- */
-
-// Note that GCC supports __int128, but claims that
-// std::is_integral_v<__int128> is false unless -std=gnuXX is passed (but we
-// disable extensions in our CMake config)
-#if COMPILER_IS_CLANG
-static_assert(flux::num::integral<__int128_t>);
-static_assert(flux::num::signed_integral<__int128_t>);
-static_assert(not flux::num::unsigned_integral<__int128_t>);
-
-static_assert(flux::num::integral<__uint128_t>);
-static_assert(not flux::num::signed_integral<__uint128_t>);
-static_assert(flux::num::unsigned_integral<__uint128_t>);
-
-// Test a couple of odd-sized custom ints as well
-using int7_t = _BitInt(7);
-using uint256_t = unsigned _BitInt(256);
-
-static_assert(flux::num::integral<int7_t>);
-static_assert(flux::num::signed_integral<int7_t>);
-static_assert(not flux::num::unsigned_integral<int7_t>);
-
-static_assert(flux::num::integral<uint256_t>);
-static_assert(not flux::num::signed_integral<uint256_t>);
-static_assert(flux::num::unsigned_integral<uint256_t>);
-#endif
