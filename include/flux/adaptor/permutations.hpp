94fa017c01a00a4fb5bebb83814ba3ad00e0a1c2
Nick Waddoups <nick.waddoups@usu.edu>
2024-11-26 23:04:56 -0700

    laid out basics of permutation adaptor, still researching impl strategies

---
+++ include/flux/adaptor/permutations.hpp
@@ -0,0 +1,48 @@
+
+// Copyright stuff here? Not really sure on that as I've never done
+// it
+
+#ifndef FLUX_ADAPTOR_PERMUTATIONS_HPP_INCLUDED
+#define FLUX_ADAPTOR_PERMUTATIONS_HPP_INCLUDED
+
+#include "flux/core/concepts.hpp"
+#include "flux/core/inline_sequence_base.hpp"
+#include "flux/macros.hpp"
+#include <cstddef>
+#include <flux/core.hpp>
+
+namespace flux {
+
+namespace detail {
+
+template <flux::sequence Base, std::size_t Length>
+    requires(Length > 0) && flux::bounded_sequence<Base> // permutation of size 0 don't make sense and infinite sequences don't make sense either
+struct permutations_adaptor : inline_sequence_base<permutations_adaptor<Base, Length>> {
+private:
+    Base base_;
+    inline static constexpr std::size_t length_ = Length;
+
+public:
+    constexpr permutations_adaptor(Base&& base) : base_(FLUX_FWD(base)) { }
+
+    struct flux_sequence_traits : default_sequence_traits {
+    private:
+        using self_t = permutations_adaptor;
+        using element_t = std::vector<flux::value_t<Base>>;
+
+        struct cursor_type {
+            /* final type is todo */ std::vector<flux::value_t<Base>> vals_;
+            /* final type is todo */ std::vector<flux::cursor_t<Base>> indices;
+            /* final type is todo */ std::vector<flux::cursor_t<Base>> cycles;
+
+            constexpr bool operator==(cursor_type const&) const = default;
+        };
+
+    public:
+        using value_type = std::vector<flux::value_t<Base>>;
+    };
+};
+} // namespace detail
+
+} // namespace flux
+#endif
